6b4e55fadb3d8ded67ee76799c7fa34ab9f4704e
S.D. <drozdi.klim@gmail.com>
2024-07-22 14:02:40 +0300

    createElement

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { createElement as h } from 'react';
 
 function App() {
-  
-  return (
-    /** Декларативный */
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <span>
-          {(new Date()).getFullYear()}
-        </span>
-      </header>
-    </div>
-  );
+  return h('div', { className: 'App' },
+    h('header', {
+      className: 'App-header'
+    }, [
+      h('img', {src: logo, className: 'App-logo', alt: 'logo'}),
+      h('p', {}, ['Edit ', h('code', {}, 'src/App.js'), ' and save to reload.']),
+      h('a', {
+        className: 'App-link',
+        href: 'https://reactjs.org',
+        target: '_blank',
+        rel: 'noopener noreferrer'
+      }, 'Learn React'),
+      h('span', {}, (new Date()).getFullYear())
+    ])
+)
 }
 
 export default App;
