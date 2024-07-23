9329cf265c3cdc0cd0b4830e7a64210613527864
S.D. <drozdi.klim@gmail.com>
2024-07-23 11:01:39 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,55 +1,78 @@
-import logo from './logo.svg';
-import style from './App.module.css';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import styles from './App.module.css';
+import data from './data.json';
 
-function App() {
-  const [value, setValue] = useState('');
-  const [list, setList] = useState([]);
-  const [error, setError] = useState(false);
-  const [isValueVaild, setIsValueVaild] = useState(false);
+export const App = () => {
+	// Можно задать 2 состояния — steps и activeIndex
+  const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  const onClick = (e) => {
-    const val = prompt('Введите значение');
-    setValue(val);
-    setError(false);
-    setIsValueVaild(true);
-    if (val?.length < 3) {
-      setError(true);
-      setIsValueVaild(false);
-    }
-  }
-  const onAddClick = () => {
-    setList([...list, {
-      value,
-      id: Date.now(),
-      date: new Date().toLocaleString()
-    }]);
-
-    setValue('');
-    setError(false);
-    setIsValueVaild(false);
+	const [isFirst, setIsFirst] = useState(activeIndex === 0);
+  const [isLast, setIsLast] = useState(activeIndex === steps.length - 1);
+	
+  /** Вариант с тремя обработчиками */
+  const onFirstCliick = () => {
+    setActiveIndex(0)
+    setIsFirst(true)
+    setIsLast(false)
   }
 
-  return (<div className={style.app}>
-    <h1 className={style['page-heading']}>Ввод значения</h1>
-    <p className={style['no-margin-text']}>
-      Текущее значение <code>value</code>: "<output className={style['current-value']}>{value}</output>"
-    </p>
-    {error && <div className={style.error}>Введенное значение должно содержать минимум 3 символа</div>}
-    <div className={style['buttons-container']}>
-      <button className={style.button} onClick={onClick}>Ввести новое</button>
-      <button className={style.button} onClick={onAddClick} disabled={!isValueVaild}>Добавить в список</button>
-    </div>
-    <div className={style['list-container']}>
-      < h2 className={style['list-heading']}>Список:</h2>
-      {!list.length && < p className={style['no-margin-text']}>Нет добавленных элементов</p>}
-      <ul className={style.list}>
-        {list.map(({ id, value, date }) => {
-          return <li key={id} className={style['list-item']}>{value} - {date}</li>
-        })}
-      </ul>
-    </div>
-  </div >)
-}
+  const onNextClick = () => {
+    const index = activeIndex+1
+    setActiveIndex(index)
+    setIsFirst(false)
+    setIsLast(index === steps.length - 1)
+  }
 
-export default App;
+  const onPreviousClick = () => {
+    const index = activeIndex-1
+    setActiveIndex(index)
+    setIsFirst(index === 0)
+    setIsLast(false)
+  }
+
+  /** 
+   * Вариант перехода по индексам, вызывать 
+   * 
+   * goIndex(0) - начало
+   * goIndex(activeIndex+1) - далее
+   * goIndex(activeIndex-1) - Назад
+   * 
+   * onFirstCliick = () => goIndex(0)
+   * onNextClick = () => goIndex(activeIndex+1)
+   * onPreviousClick = () => goIndex(activeIndex-1)
+   * */
+	const goStep = (index) => {
+    setActiveIndex(index)
+    setIsFirst(index === 0)
+    setIsLast(index === steps.length - 1)
+  }
+	
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+            {steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						{steps.map(({id, title, content}, index) => (
+              <li className={styles['steps-item'] + (index<=activeIndex? ' '+styles.done: '') + (index===activeIndex? ' '+styles.active: '')} key={id} onClick={() => setActiveIndex(index)}>
+                <button className={styles['steps-item-button']} onClick={() => goStep(index)}>{index + 1}</button>
+                {title}
+              </li>
+            ))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button} disabled={isFirst} onClick={onPreviousClick}>Назад</button>
+						{isLast? 
+              <button className={styles.button} onClick={onFirstCliick}>Начать сначала</button>:
+              <button className={styles.button} onClick={onNextClick}>Далее</button>}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
