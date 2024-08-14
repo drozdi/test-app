40261421d1fae1b611fa102bee56933fb9ae586f
Drozdi89 <drozdi-klim@yandex.ru>
2024-08-14 14:51:42 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import style from './App.module.css';
+import styles from './App.module.css';
 import React, { useState, useRef } from 'react';
 
 function sendFormData(formData) {
@@ -8,79 +8,78 @@ function sendFormData(formData) {
 
 const rules = {
   login: [
-    v => !!v || 'Логин обязателен',
-    v => /^[\w_]*$/.test(v) || 'Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание',
-    v => v.length > 2 || 'Неверный логин. Должно быть не меньше 3 символов',
-    v => v.length < 21 || 'Неверный логин. Должно быть не больше 20 символов',
-  ]
+    v => !!v || 'Логин обязателен.',
+    v => /^[\w_]*$/.test(v) || 'Допустимые символы: буквы, цифры и нижнее подчёркивание.',
+    v => v.length > 2 || 'Должно быть не меньше 3 символов.',
+    v => v.length < 21 || 'Должно быть не больше 20 символов.',
+  ],
+  password: [],
+  re_password: []
 }
 
 function App() {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [rePassword, setRePassword] = useState('');
-  const [errors, setErrors] = useState([]);
-  const submit = useRef(null)
+  const [formData, setFormData] = useState({
+    login: '',
+    password: '',
+    re_password: ''
+  });
+  const [errors, setErrors] = useState({
+    login: [],
+    password: [],
+    re_password: []
+  });
+  const [validate, setValidate] = useState(false);
+  const submitRef = useRef(null)
 
   const validation = (target) => {
-    return (rules[target.name] || []).map(rule => rule(target.value)).filter(v => v !== true);
+    const err = (rules[target.name] || []).map(rule => rule(target.value)).filter(v => v !== true);
+    if (err.length) {
+      setValidate(false);
+    }
+    return err;
   }
 
-  const onLoginChange = ({ target }) => {
-    setLogin(target.value);
-    setErrors(validation(target));
-  };
-  const onPasswordChange = ({ target }) => {
-    setPassword(target.value);
-    const newError = validation(target);
-    if (rePassword && rePassword !== target.value) {
-      newError.push('Пароли должны совпадать!');
-    }
-    setErrors(newError);
-  };
-  const onRePasswordChange = ({ target }) => {
-    setRePassword(target.value);
-    if (password === target.value) {
-      setErrors([]);
-      submit.current.focus();
-    } else {
-      setErrors(['Пароли должны совпадать!']);
-    }
-  };
+  const handleChange = ({ target }) => {
+    setValidate(true);
+    setFormData({ ...formData, [target.name]: target.value });
+    setErrors({ ...errors, [target.name]: validation(target) })
+  }
+
+
+
 
   const onSubmit = (event) => {
     event.preventDefault();
-    sendFormData({ login, password, rePassword });
+    sendFormData({ ...formData });
   };
 
-  return (<div className={style.app}>
-    <form onSubmit={onSubmit} className={style.form}>
-      {errors.map((mes, index) => <div key={index} className={style.error}>{mes}</div>)}
+  return (<div className={styles.app}>
+    <form onSubmit={onSubmit} className={styles.form}>
       <input
         name="login"
         type="text"
-        value={login}
-        onChange={onLoginChange}
-        className={style.input}
+        value={formData.login}
+        onChange={handleChange}
+        className={styles.input}
         placeholder="Логин" />
-
+      {errors.login.map((mes, index) => <div key={index} className={styles.error}>{mes}</div>)}
       <input
         name="password"
         type="password"
-        value={password}
-        onChange={onPasswordChange}
-        className={style.input}
+        value={formData.password}
+        onChange={handleChange}
+        className={styles.input}
         placeholder="Пароль" />
-
+      {errors.password.map((mes, index) => <div key={index} className={styles.error}>{mes}</div>)}
       <input
         name="re_password"
         type="password"
-        value={rePassword}
-        onChange={onRePasswordChange}
-        className={style.input}
+        value={formData.re_password}
+        onChange={handleChange}
+        className={styles.input}
         placeholder="Повторить пароль" />
-
-      <button ref={submit} className={style.button} type="submit" disabled={errors.length}>Зарегистрировать</button>
+      {errors.re_password.map((mes, index) => <div key={index} className={styles.error}>{mes}</div>)}
+      <button ref={submitRef} className={styles.button} type="submit" disabled={!validate}>Зарегистрировать</button>
     </form>
   </div>)
 }
