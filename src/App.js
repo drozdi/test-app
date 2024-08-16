b0e0d51ff3528980fcfa7abd31eb4c6d5fe1ed2d
Drozdi89 <drozdi-klim@yandex.ru>
2024-08-16 16:49:28 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styles from './App.module.css';
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo, useEffect } from 'react';
 
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -14,7 +14,7 @@ const fieldsSchema = yup.object()
   .shape({
     login: yup.string()
       .required('Логин обязателен.')
-      .matches(/^[w_]*$/, 'Допустимые символы: буквы, цифры и нижнее подчёркивание')
+      .matches(/^[\w_]*$/, 'Допустимые символы: буквы, цифры и нижнее подчёркивание')
       .min(3, 'Должно быть не меньше 3 символов')
       .max(20, 'Должно быть не больше 20 символов'),
     password: yup.string()
@@ -22,7 +22,7 @@ const fieldsSchema = yup.object()
       .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]/, 'Допустимые символы: буквы, цифры и спец. символы @#$%^&*')
       .min(6, 'Должно быть не меньше 6 символов')
       .max(16, 'Должно быть не больше 16 символов'),
-    confirmPassword: yup.string()
+    re_password: yup.string()
       .required('Повтор пароль обязателен.')
       .oneOf([yup.ref('password')], 'Пароли должны совпадать!')
   });
@@ -31,8 +31,10 @@ function App() {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
+    mode: 'onChange',
     defaultValues: {
       login: '',
       password: '',
@@ -41,14 +43,20 @@ function App() {
     resolver: yupResolver(fieldsSchema),
   });
 
-
   const submitRef = useRef(null);
 
-  // наверно можно как то подругому, но это получаеться удобно
-  const validate = useMemo(() => {
-    return !(errors.login?.message || errors.password?.message || errors.re_password?.message);
-  }, [errors]);
 
+  useEffect(() => {
+    const login = watch('login');
+    const password = watch('password');
+    const re_password = watch('re_password');
+    if (login && password && password === re_password) {
+      submitRef.current.disabled = false;
+      submitRef.current.focus();
+    } else {
+      submitRef.current.disabled = true;
+    }
+  }, [errors.login, errors.password, errors.re_password]);
 
   return (<div className={styles.app}>
     <form onSubmit={handleSubmit(sendFormData)} className={styles.form}>
@@ -58,22 +66,22 @@ function App() {
         {...register('login')}
         className={styles.input}
         placeholder="Логин" />
-      {errors.login && <div className={styles.error}>{errors.login}</div>}
+      {errors.login && <div className={styles.error}>{errors.login?.message}</div>}
       <input
         name="password"
         type="password"
         {...register('password')}
         className={styles.input}
         placeholder="Пароль" />
-      {errors.password && <div className={styles.error}>{errors.password}</div>}
+      {errors.password && <div className={styles.error}>{errors.passwor?.messaged}</div>}
       <input
         name="re_password"
         type="password"
         {...register('re_password')}
         className={styles.input}
         placeholder="Повторить пароль" />
-      {errors.re_password && <div className={styles.error}>{errors.re_password}</div>}
-      <button ref={submitRef} className={styles.button} type="submit" disabled={!validate}>Зарегистрировать</button>
+      {errors.re_password && <div className={styles.error}>{errors.re_password?.message}</div>}
+      <button ref={submitRef} className={styles.button} type="submit" disabled={true}>Зарегистрировать</button>
     </form>
   </div>)
 }
