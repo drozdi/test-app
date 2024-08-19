87d2ef29ea7bbe8a054a8be2acbf125ac1693142
Drozdi <drozdi.klim@gmail.com>
2024-08-19 18:19:50 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 
-import Button from '@mui/material/Button';
 
 import { XLayout, XHeader, XFooter, XSideBar, XMain } from "./components/ui/layout/XLayout";
 
 function App() {
   return (<XLayout container={true} view="hhr lbr lff">
     <XHeader>
-      <Button variant="contained" color="primary">primary</Button>
+      
     </XHeader>
-    <XSideBar open={false} type='left'>left</XSideBar>
-    <XSideBar open={false} type='right'>right</XSideBar>
+    <XSideBar type='left'>left</XSideBar>
+    <XSideBar type='right'>right</XSideBar>
     <XMain>
       <h1>body</h1>
     </XMain>
--- src/components/ui/layout/XLayout.jsx
+++ src/components/ui/layout/XLayout.jsx
@@ -4,7 +4,7 @@ import { useResizeObserver } from "../../../hooks/useResizeObserver";
 
 import { styled, useTheme } from '@mui/material/styles';
 import Drawer from '@mui/material/Drawer';
-
+import Button from '@mui/material/Button';
 
 export const LayoutContext = createContext(null);
 
@@ -15,9 +15,9 @@ export function XLayout({ children, container = false, view = 'hhh lpr fff' }) {
             return row.split('')
         }),
         header: { size: 0, offset: 0, space: false },
-        right: { size: 0, offset: 0, space: false },
+        right: { size: 0, offset: 0, space: false, open: false },
         footer: { size: 0, offset: 0, space: false },
-        left: { size: 0, offset: 0, space: false },
+        left: { size: 0, offset: 0, space: false, open: false },
     });
     const $update = (part, prop, val) => {
         if ($layout[part][prop] !== val) {
@@ -29,7 +29,7 @@ export function XLayout({ children, container = false, view = 'hhh lpr fff' }) {
             }))
         }
     }
-
+    
     const {
         XHeader = null,
         XFooter = null,
@@ -58,7 +58,7 @@ export function XLayout({ children, container = false, view = 'hhh lpr fff' }) {
         }
         return res;
     })(children)
-
+    
     let layout = (<div className="x-layout">
         {XHeader}
         {XFooter}
@@ -70,101 +70,103 @@ export function XLayout({ children, container = false, view = 'hhh lpr fff' }) {
             paddingLeft: $layout.left.size,
             paddingRight: $layout.right.size
         }}>
-            {XMain}
+        {XMain}
         </div>
-    </div>)
-    if (container) {
-        layout = (<div className="x-layout__container">
-            {layout}
         </div>)
-    }
-
-    return (<LayoutContext.Provider value={{ $layout, $update }}>
-        {layout}
-    </LayoutContext.Provider>)
-}
-
-export function XHeader({ children, className = '' }) {
-    const { $layout, $update } = useContext(LayoutContext)
-    console.log($layout)
-    let cn = ['x-header', $layout ? ' x-layout__header' : '', className].join(' ')
-    const ref = useResizeObserver((target, entry) => {
-        $update('header', 'size', target.offsetHeight);
-    });
-
-    const style = useMemo(() => {
-        const css = {};
-        if ($layout.rows[0][0] === "l") {
-            css.left = $layout.left.size;
-        }
-        if ($layout.rows[0][2] === "r") {
-            css.right = $layout.right.size;
-        }
-        return css;
-    }, [$layout])
-    return (<header ref={ref} className={cn} style={style}>
-        {children}
-    </header>)
-}
-
-export function XFooter({ children, className = '' }) {
-    const { $layout, $update } = useContext(LayoutContext)
-    let cn = ['x-footer', $layout ? ' x-layout__footer' : '', className].join(' ')
-    const ref = useResizeObserver((target, entry) => {
-        $update('footer', 'size', target.offsetHeight)
-    })
-    const style = useMemo(() => {
-        const css = {};
-        if ($layout.rows[2][0] === "l") {
-            css.left = $layout.left.size;
-        }
-        if ($layout.rows[2][2] === "r") {
-            css.right = $layout.right.size;
-        }
-        return css;
-    }, [$layout])
-    return (<footer ref={ref} className={cn} style={style}>
-        {children}
-    </footer>)
-}
-
-const openedMixin = (theme, drawerWidth) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-    
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    minWidth: 0,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
- });
-
-
+        if (container) {
+            layout = (<div className="x-layout__container">
+                {layout}
+                </div>)
+            }
+            
+            return (<LayoutContext.Provider value={{ $layout, $update }}>
+                {layout}
+                </LayoutContext.Provider>)
+            }
+            
+            export function XHeader({ children, className = '' }) {
+                const { $layout, $update } = useContext(LayoutContext)
+                console.log($layout)
+                let cn = ['x-header', $layout ? ' x-layout__header' : '', className].join(' ')
+                const ref = useResizeObserver((target, entry) => {
+                    $update('header', 'size', target.offsetHeight);
+                });
+                const onClick = (e) => {
+                    $update('left', 'open', !$layout.left.open);
+                    
+                }
+                
+                const style = useMemo(() => {
+                    const css = {};
+                    if ($layout.rows[0][0] === "l") {
+                        css.left = $layout.left.size;
+                    }
+                    if ($layout.rows[0][2] === "r") {
+                        css.right = $layout.right.size;
+                    }
+                    return css;
+                }, [$layout])
+                return (<header ref={ref} className={cn} style={style}>
+                    <Button variant="contained" color="primary" onClick={onClick}>primary</Button>
+                    {children}
+                    </header>)
+                }
+                
+                export function XFooter({ children, className = '' }) {
+                    const { $layout, $update } = useContext(LayoutContext)
+                    let cn = ['x-footer', $layout ? ' x-layout__footer' : '', className].join(' ')
+                    const ref = useResizeObserver((target, entry) => {
+                        $update('footer', 'size', target.offsetHeight)
+                    })
+                    const style = useMemo(() => {
+                        const css = {};
+                        if ($layout.rows[2][0] === "l") {
+                            css.left = $layout.left.size;
+                        }
+                        if ($layout.rows[2][2] === "r") {
+                            css.right = $layout.right.size;
+                        }
+                        return css;
+                    }, [$layout])
+                    return (<footer ref={ref} className={cn} style={style}>
+                        {children}
+                        </footer>)
+                    }
+                    
+                    const openedMixin = (theme, width) => ({
+                        width: width, minWidth: 0,
+                    });
+                    const closedMixin = (theme) => ({
+                        width: `calc(${theme.spacing(7)} + 1px)`,
+                        minWidth: 0,
+                        [theme.breakpoints.up('sm')]: {
+                            width: `calc(${theme.spacing(8)} + 1px)`,
+                        },
+                    });
+                    
+                    
 export function XSideBar({ children, className, open=true, type = 'left', width = 200 }) {
     const theme = useTheme();
+                        
     const { $layout, $update } = useContext(LayoutContext)
+                        
+    const opened = $layout? $layout[type].open: open;
     let cl = ['x-sidebar', $layout ? ' x-layout__sidebar x-layout__sidebar--' + type : '', className].join(' ')
-
+                        
     const ref = useResizeObserver((target, entry) => {
         $update(type, 'size', target.offsetWidth);
     });
-
+                        
     const header = $layout.rows[0][type === 'left' ? 0 : 2] === type[0]
     const footer = $layout.rows[2][type === 'left' ? 0 : 2] === type[0]
-
+                        
     const style = useMemo(() => ({
-        ...(open? openedMixin: closedMixin)(theme, width),
+        ...(opened? openedMixin: closedMixin)(theme, width),
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        overflowX: 'hidden',
         top: header ? 0 : $layout.header.size,
         bottom: footer ? 0 : $layout.footer.size,
         position: 'absolute',
@@ -176,32 +178,18 @@ export function XSideBar({ children, className, open=true, type = 'left', width 
             bottom: 0,
             left: 0,
             right: 0,
-        },
-    }
-        /*{
-        top: header ? 0 : $layout.header.size,
-        bottom: footer ? 0 : $layout.footer.size,
-        width: width,
-        position: 'absolute',
-        [type]: 0,
-        [`& .MuiDrawer-paper`]: { 
-            boxSizing: 'border-box',
-            position: 'absolute',
-            width: width,
-        },
-    }*/
-), [$layout, width, open])
-
-    return (<Drawer ref={ref} className={cl} open={true} anchor={type} variant="permanent" sx={style}>
+        }
+    }), [$layout, width, opened])
+                    
+    return (<Drawer ref={ref} className={cl} elevation={0} open={opened} anchor={type} variant="permanent" sx={style}>
         {children}
     </Drawer>)
-    /*return (<aside ref={ref} className={cn} style={style}>
-        {children}
-    </aside>)//*/
 }
-
+                    
 export function XMain({ children }) {
+    const { $layout } = useContext(LayoutContext)
     return (<main className="x-layout__main">
         {children}
     </main>)
 }
+                        
