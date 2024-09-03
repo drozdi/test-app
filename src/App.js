1968523ed26774a7671cf382b1b4d0250bf91ef5
Drozdi89 <drozdi.klim@gmail.com>
2024-09-03 14:21:30 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { XSidebar } from "./components/ui/sidebar/XSidebar";
 
 function App() {
-    return (<XSidebar collapsing={true} collapsedForOverlay={true} type="left">
-      <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-      </ul>
-    </XSidebar>);
+    const [mini, setMini] = useState(false);
+    const [open, setOpen] = useState(false);
+    return (<><XSidebar 
+        type="left"
+        mini={mini} 
+        open={open}
+        miniToOverlay={true} 
+        onMouseEnter={() => setMini(false)} 
+        onMouseLeave={() => setMini(true)}>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+      </XSidebar>
+      <div className='flex flex-col' style={{
+        position: 'fixed',
+        right: 20,
+        top: 20
+      }}>
+        <button className='bg-dimmed px-3 py-4 border border-separate' onClick={() => setMini(!mini)}>mini: {mini? 'true': 'false'}</button>
+        <button className='bg-dimmed px-3 py-4 border border-separate mt-3' onClick={() => setOpen(!open)}>open: {open? 'true': 'false'}</button>
+      </div>
+    </>);
   }
   
   export default App;
--- src/components/ui/sidebar/XSidebar.jsx
+++ src/components/ui/sidebar/XSidebar.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useResizeObserver } from "../../../hooks/useResizeObserver";
 import styles from './XSidebar.module.scss';
 import { XSidebarContext } from './XSidebarContext';
@@ -9,43 +9,72 @@ export function XSidebar({
 
     type = null,
     
-    collapsed = false, 
-    collapsedBreakpoint = null,
-    collapsedForOverlay = false,
+    mini = false, 
+    miniToOverlay = false,
 
     open = false,
-    breakpoint = null,
+    overlay = false,
+    breakpoint = 600,
 
     width = 300,
     minWidth = 56,
     maxWidth = 400,
+
+    onMouseEnter = () => {},
+    onMouseLeave = () => {},
     ...props
 }) {
-
+    const [$layout, $update] = useState({
+        totalWidth: 1000,
+    })
     const containerRef = useResizeObserver((target, entry) => {
+        //$update(old => ({...old, totalWidth: target.offsetWidth}));
         //$update('footer', 'size', target.offsetHeight)
     })
+
+    function resss () {
+        $update(old => ({...old, totalWidth: window.innerWidth}));
+    }
+    useEffect(() => {
+        window.addEventListener('resize', resss);
+        resss();
+        return () => {
+            window.removeEventListener('resize', resss);
+        }
+    }, []);
     
-    const isCollapsed = useMemo(() => collapsed || (collapsedBreakpoint && (window.innerWidth < collapsedBreakpoint)), [collapsed, collapsedBreakpoint])
 
-    const isOpen = useMemo(() => open || (breakpoint && (window.innerWidth < breakpoint)), [breakpoint, open])
+    const belowBreakpoint = useMemo(() => 
+        (breakpoint && $layout.totalWidth < breakpoint) || false, [$layout, breakpoint]);
+    
 
 
-    const style = useMemo(() => {
+
+    const isMini = useMemo(() => mini && !belowBreakpoint, [mini, belowBreakpoint])
+    const isOpen = useMemo(() => 
+        belowBreakpoint? false :open, [open, belowBreakpoint])
+    console.log(isOpen)
+    const isOverlay = useMemo(() => overlay, [overlay])
+    const isMiniToOverlay = useMemo(() => miniToOverlay, [miniToOverlay])
+
+
+    const containerStyle = useMemo(() => {
         return {
+            display: isOpen? 'flex' : 'none',
             /*width: collapsing? null: isCollapsed ? minWidth : width,
             maxWidth: maxWidth*/
         }
-    }, [width, maxWidth, minWidth, isCollapsed])
+    }, [isOpen])
 
-    return (<XSidebarContext.Provider value={{ isCollapsed }}>
+    return (<XSidebarContext.Provider value={{ isMini }}>
         <div ref={containerRef} className={classNames(styles.xSidebar__container, {
             [styles[`xSidebar--${type}`]]: !!type,
-            [styles['xSidebar__container--collapsed']]: isCollapsed,
-            [styles['xSidebar__container--overlay']]: collapsedForOverlay,
-        })} style={style}>
-            <div className={classNames(styles.xSidebar, {
+            [styles['xSidebar__container--mini']]: isMini,
+            [styles['xSidebar__container--overlay']]: isMiniToOverlay,
+        })} style={containerStyle}>
+            <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className={classNames(styles.xSidebar, {
                 [styles[`xSidebar--${type}`]]: !!type,
+                [styles[`xSidebar--overlay`]]: isOverlay,
             })}>
                 <div className={styles.xSidebar__content}>
                     {children}
