90bf62bfba180400c933220515bef3f2c8879e6e
Drozdi89 <drozdi.klim@gmail.com>
2024-09-23 12:49:33 +0300

    -

--- src/components/ui/window/XWindow.js
+++ src/components/ui/window/XWindow.js
@@ -1,12 +1,25 @@
 import classNames from 'classnames';
+import { Resizable } from 'react-resizable';
 import './XWindow.scss';
-export function XWindow({ children, className, x, y, w, h, title }) {
+export function XWindow({ children, className, x, y, w, h, title, resizable = true }) {
 	return (
-		<div className={classNames('xWindow', className)}>
-			<div className="xWindow-bar">
-				{title && <div className="xWindow-title">{title}</div>}
+		<Resizable
+			width={200}
+			height={200}
+			handle={(handleAxis) => (
+				<div className={`xWindow-res xWindow-res--${handleAxis}`} />
+			)}
+		>
+			<div
+				className={classNames('xWindow', className, {
+					'xWindow--resizable': resizable,
+				})}
+			>
+				<div className="xWindow-bar">
+					{title && <div className="xWindow-title">{title}</div>}
+				</div>
+				<div className="xWindow-content">{children}</div>
 			</div>
-			<div className="xWindow-content">{children}</div>
-		</div>
+		</Resizable>
 	);
 }
