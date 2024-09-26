41190a10adaaf3cd680674ab322f60381958a058
Drozdi89 <drozdi.klim@gmail.com>
2024-09-26 15:15:22 +0300

    -

--- src/hooks/useProxy.js
+++ src/hooks/useProxy.js
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export function useProxy (defState = {}) {
-    const [state, setState] = useState(defState);
-    return new Proxy(state, {
-        get (target, property) {
-            if (property in target) {
-                return target[property];
-            } else {
-                return undefined;
-            }
-        },
-        set (target, property, value) {
-            setState(v => ({ ...v, [property]: value}));
-            target[property] = value;
-            return true;
-        },
-        deleteProperty (target, property) { // перехватываем удаление свойства
-            setState(v => {
-                let newV = {...v};
-                delete newV[property];
-                return newV;
-            });
-            delete target[property];
-            return true;
-        },
-    });
+export function useProxy(initiState = {}) {
+	const [state, setState] = useState(initiState);
+	return new Proxy(state, {
+		get(target, property) {
+			if (property in target) {
+				return target[property];
+			} else {
+				return undefined;
+			}
+		},
+		set(target, property, value) {
+			setState((v) => ({ ...v, [property]: value }));
+			target[property] = value;
+			return true;
+		},
+		deleteProperty(target, property) {
+			// перехватываем удаление свойства
+			setState((v) => {
+				let newV = { ...v };
+				delete newV[property];
+				return newV;
+			});
+			delete target[property];
+			return true;
+		},
+	});
 }
