7427193aae1fc1483055684d99c692449bd4077a
Drozdi89 <drozdi.klim@gmail.com>
2024-10-16 16:37:03 +0300

    -

---
+++ src/hooks/useInput.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+const validation = (value, rules = []) => {
+	return rules.map((rule) => rule(value)).filter((v) => v !== true);
+};
+export const useInput = (initialValue, rules = []) => {
+	const [value, setValue] = useState(initialValue);
+	const [dirty, setDirty] = useState(false);
+	const [errors, setErrors] = useState(validation(value, rules));
+
+	return {
+		value,
+		dirty,
+		errors,
+		onBlur: (e) => {
+			setDirty(true);
+		},
+		onChange: ({ target }) => {
+			setValue(target.value);
+			setErrors(validation(target.value, rules));
+		},
+	};
+};
