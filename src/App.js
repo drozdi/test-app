1e86a81f6317755a11f18569c7749cfb246444ba
Drozdi <drozdi.klim@gmail.com>
2024-10-28 16:42:45 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
-import Example from './components/example';
+import  { BtnExample} from './components/example';
+import { XLayout } from'./components/ui/layout';
+import { XList, XItem, XItemSection, XItemLabel } from'./components/ui';
+import { Route, Link } from 'react-router-dom';
 function App() {
-	return <Example list={true} btnGroup={true} btn={true} input={true} />;
+  return (<XLayout container={true} overlay={true} view="hhr lpr lff">
+      {{
+							left: (props) => (
+								<XList>
+                  <XItem to="/btn" LinkComponent={Link}>
+                    <XItemSection side={true}>
+                      <XIcon>mdi-home</XIcon>
+                    </XItemSection>
+                    <XItemSection>Yandex</XItemSection>
+                  </XItem>
+                </XList>
+							),
+							right: (props) => (
+								<>right</>
+							),
+							header: 'header',
+							footer: 'footer',
+							default: (props) => (
+								<Routes>
+                  <Route path="/"  element={<div>Home</div>} />
+                  <Route path="/btn" element={<BtnExample />} />
+                </Routes>
+							),
+						}}
+
+  </XLayout>)
+  
+  /*
+        
+        <Route path="/todo/:id" element={<TodoPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>*/
+	//return <Example list={true} btnGroup={true} btn={true} input={true} />;
 	/*const ref = useRef(null);
 	useEffect(() => {
 		//console.log(ref);
