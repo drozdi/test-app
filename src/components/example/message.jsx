0ae180be87471ecc336b3864bd0569563d1e9466
Drozdi <drozdi-klim@yandex.ru>
2024-11-06 18:33:06 +0300

    -

--- src/components/example/message.jsx
+++ src/components/example/message.jsx
@@ -1,10 +1,44 @@
+import { useState } from 'react';
 import { XMessage } from '../ui/message';
 export function MessageExample() {
+	const [mess, setMess] = useState([
+		{
+			children: 'Test 1',
+			id: 1,
+			color: 'info',
+			outline: true,
+			underline: 'left',
+			icon: 'mdi-close',
+			onClose: onRemove,
+		},
+		{
+			children: 'Test 2',
+			id: 2,
+			color: 'info',
+			outline: true,
+			icon: 'mdi-close',
+			onClose: onRemove,
+		},
+		{
+			children: 'Test 3',
+			id: 3,
+			color: 'info',
+			outline: true,
+			icon: 'mdi-close',
+			onClose: onRemove,
+		},
+	]);
+	function onRemove({ id }) {
+		const n = mess.filter((item) => item.id !== id);
+		setMess(n);
+	}
 	return (
 		<div className="max-w-4xl m-auto py-4 relative">
-			<XMessage type="info" underlined="left" outline={true} icon="mdi-close">
-				Test
-			</XMessage>
+			<div className="flex flex-col gap-4">
+				{mess.map((item) => (
+					<XMessage key={item.id} {...item} />
+				))}
+			</div>
 		</div>
 	);
 }
--- src/components/ui/message/XMessage.jsx
+++ src/components/ui/message/XMessage.jsx
@@ -1,13 +1,17 @@
 import classNames from 'classnames';
-import { forwardRef, useMemo, useRef } from 'react';
+import { forwardRef, useCallback, useMemo, useRef } from 'react';
 import { isString } from '../../../utils/is';
 import { XBtn } from '../btn';
 import { XIcon } from '../icon';
 import './style.scss';
+import { useXMessagesContext } from './XMessagesContext';
+
 export const XMessage = forwardRef(function XMessage(
-	{ id, className, icon, type, children, style, outline, square, underlined },
+	{ id, className, icon, color, children, style, outline, square, underlined, onClose },
 	ref,
 ) {
+	const context = useXMessagesContext();
+	const canClose = true;
 	const elementRef = useRef(null);
 	const Icon = useMemo(() => {
 		if (icon?.type === XIcon) {
@@ -28,11 +32,22 @@ export const XMessage = forwardRef(function XMessage(
 		[style],
 	);
 
+	const handleClose = useCallback(
+		(event) => {
+			onClose && onClose({ id });
+			if (event) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+		},
+		[onClose],
+	);
+
 	return (
 		<div
 			{...attrs}
 			className={classNames('x-message', {
-				[`x-message--${type}`]: type,
+				[`x-message--${color}`]: color,
 				'x-message--square': square,
 				'x-message--outline': outline,
 				[`x-message--underlined-${underlined}`]: underlined,
@@ -45,9 +60,17 @@ export const XMessage = forwardRef(function XMessage(
 				{Icon}
 				{children}
 			</div>
-			<div className="x-message__close">
-				<XBtn icon="mdi-close" flat={true} size="xs" plain={true} />
-			</div>
+			{canClose && (
+				<div className="x-message__close">
+					<XBtn
+						icon="mdi-close"
+						size="xs"
+						flat={true}
+						plain={true}
+						onClick={handleClose}
+					/>
+				</div>
+			)}
 		</div>
 	);
 });
