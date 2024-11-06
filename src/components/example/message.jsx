15dd3c9c1d33520d809f1506f517f2e1de2461a6
Drozdi89 <drozdi.klim@gmail.com>
2024-11-06 15:49:52 +0300

    -

--- src/components/example/message.jsx
+++ src/components/example/message.jsx
@@ -1,3 +1,10 @@
+import { XMessage } from '../ui/message';
 export function MessageExample() {
-	return <div className="max-w-4xl m-auto py-4 relative"></div>;
+	return (
+		<div className="max-w-4xl m-auto py-4 relative">
+			<XMessage type="info" underlined="left" outline={true} icon="mdi-close">
+				Test
+			</XMessage>
+		</div>
+	);
 }
--- src/components/ui/message/XMessage.jsx
+++ src/components/ui/message/XMessage.jsx
@@ -1,9 +1,53 @@
-import { forwardRef, useRef } from 'react';
-
+import classNames from 'classnames';
+import { forwardRef, useMemo, useRef } from 'react';
+import { isString } from '../../../utils/is';
+import { XBtn } from '../btn';
+import { XIcon } from '../icon';
+import './style.scss';
 export const XMessage = forwardRef(function XMessage(
-	{ id, className, text, icon, type, content, children },
+	{ id, className, icon, type, children, style, outline, square, underlined },
 	ref,
 ) {
 	const elementRef = useRef(null);
-	return <div ref={elementRef}>XMessage</div>;
+	const Icon = useMemo(() => {
+		if (icon?.type === XIcon) {
+			return icon;
+		} else if (isString(icon)) {
+			return <XIcon>{icon}</XIcon>;
+		}
+	}, [icon]);
+
+	const attrs = useMemo(
+		() => ({
+			id,
+			style,
+			role: 'alert',
+			'aria-live': 'polite',
+			'aria-atomic': 'true',
+		}),
+		[style],
+	);
+
+	return (
+		<div
+			{...attrs}
+			className={classNames('x-message', {
+				[`x-message--${type}`]: type,
+				'x-message--square': square,
+				'x-message--outline': outline,
+				[`x-message--underlined-${underlined}`]: underlined,
+			})}
+			ref={elementRef}
+		>
+			<div className="x-message__backdor" />
+			<div className="x-message__outline" />
+			<div className={classNames('x-message__content', className)}>
+				{Icon}
+				{children}
+			</div>
+			<div className="x-message__close">
+				<XBtn icon="mdi-close" flat={true} size="xs" plain={true} />
+			</div>
+		</div>
+	);
 });
