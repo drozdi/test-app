ce82cce550b1cfeb49d9ebdaa68fb62b4bfab66d
Drozdi89 <drozdi.klim@gmail.com>
2024-11-21 16:04:09 +0300

    -

--- src/components/ui/toast/XToastContext.js
+++ src/components/ui/toast/XToastContext.js
@@ -3,5 +3,6 @@ import { createContext, useContext } from 'react';
 export const XToastContext = createContext(null);
 
 export function useXToastContext() {
-	return useContext(XToastContext);
+	const context = useContext(XToastContext);
+	return context;
 }
