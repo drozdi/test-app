5e0a9b6c8584b9bb71d4100996acad7a3d651cd3
Drozdi <drozdi-klim@yandex.ru>
2024-11-21 22:22:24 +0300

    -

--- src/components/ui/progress/XProgressBar.jsx
+++ src/components/ui/progress/XProgressBar.jsx
@@ -8,10 +8,12 @@ export function XProgressBar({
 	buffer,
 	trackColor,
 	color,
+	label,
 	stripe,
 	animation,
 	indeterminate,
 	reverse,
+	...props
 }) {
 	const attrs = useMemo(() => ({
 		role: 'progressbar',
@@ -19,25 +21,39 @@ export function XProgressBar({
 		'aria-valuemax': 100,
 		'aria-valuenow': indeterminate === true ? void 0 : value,
 	}));
-	const trackAttrs = useMemo(() => ({
-		style: { width: buffer && !indeterminate ? `${buffer}%` : '' },
-	}));
-	const valueAttrs = useMemo(() => ({
-		style: { width: value && !indeterminate ? `${value}%` : '' },
-	}));
+	const trackAttrs = useMemo(
+		() => ({
+			style: { width: buffer && !indeterminate ? `${buffer}%` : '' },
+		}),
+		[buffer, indeterminate],
+	);
+	const valueAttrs = useMemo(
+		() => ({
+			style: { width: value && !indeterminate ? `${value}%` : '' },
+		}),
+		[value, indeterminate],
+	);
+
 	return (
 		<div
+			{...props}
 			{...attrs}
 			className={classNames('x-progress-bar', className, {
 				'x-progress-bar--stripe': !indeterminate && stripe,
 				'x-progress-bar--animation': !indeterminate && animation,
 				'x-progress-bar--indeterminate': indeterminate,
 				'x-progress-bar--reverse': reverse,
+				[`x-progress-bar--${color}`]: color,
 			})}
 		>
 			<div {...trackAttrs} className="x-progress-bar__track"></div>
 			<div {...valueAttrs} className="x-progress-bar__value"></div>
-			{children && <div className="x-progress-bar__label">{children}</div>}
+			{!indeterminate && children && (
+				<div className="x-progress-bar__label">{children}</div>
+			)}
+			{!indeterminate && !children && label && (
+				<div className="x-progress-bar__label">{value}%</div>
+			)}
 		</div>
 	);
 }
