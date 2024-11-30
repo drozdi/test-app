1c197ab423867e2a4a0bff4d33fa515dd72e2c76
Drozdi <drozdi-klim@yandex.ru>
2024-11-30 15:23:12 +0300

    -

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
+import { extractEventHandlers } from '../utils/extractEventHandlers';
 const validation = (value, rules = []) => {
 	return rules.map((rule) => rule(value)).filter((v) => v !== true);
 };
@@ -7,28 +8,39 @@ export const useInput = (parameters = {}) => {
 		initialValue,
 		disabled = false,
 		error = false,
-		onBlur,
-		onChange,
-		onFocus,
 		rules = [],
 		required: requiredProp = false,
 	} = parameters;
-
-	const inputRef = useRef(null);
+	const externalEventHandlers = {
+		...extractEventHandlers(parameters),
+	};
 	const [value, setValue] = useState(initialValue);
 	const [dirty, setDirty] = useState(false);
 	const [errors, setErrors] = useState(validation(value, rules));
+	const [focus, setFocus] = useState(false);
+
+	const createHandleBlur = (otherHandlers) => (event) => {
+		setFocus(false);
+		setDirty(true);
+		otherHandlers.onBlur?.(event);
+	};
+	const createHandleFocus = (otherHandlers) => (event) => {
+		setFocus(true);
+		otherHandlers.onFocus?.(event);
+	};
+	const createHandleChange = (otherHandlers) => (event) => {
+		setValue(e.target.value);
+		setErrors(validation(e.target.value, rules));
+		otherHandlers.onChange?.(event);
+	};
 
 	return {
 		value,
 		dirty,
 		errors,
-		onBlur: (e) => {
-			setDirty(true);
-		},
-		onChange: ({ target }) => {
-			setValue(target.value);
-			setErrors(validation(target.value, rules));
-		},
+		focus,
+		onBlur: createHandleBlur(externalEventHandlers),
+		onFocus: createHandleFocus(externalEventHandlers),
+		onChange: createHandleChange(externalEventHandlers),
 	};
 };
