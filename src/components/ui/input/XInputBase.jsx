dc96518fcfefa28f0ce8cbe13f5bba499e1743ff
Drozdi <drozdi-klim@yandex.ru>
2025-01-28 17:06:22 +0300

    -

--- src/components/ui/input/XInputBase.jsx
+++ src/components/ui/input/XInputBase.jsx
@@ -11,6 +11,7 @@ const XInputBaseRoot = forwardRef(function XInputBaseFn(
 		placeholder,
 		color,
 		labelColor,
+		stackLabel,
 		leftSection: propsLeftSection,
 		rightSection: propsRightSection,
 	},
